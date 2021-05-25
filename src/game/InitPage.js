33a3306a92cf80c9944fbceaffeb180f329586e8
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-25 09:18:18 -0700

    Printing categories on the init page

--- src/game/InitPage.js
+++ src/game/InitPage.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import { categories } from '../data/categories.js';
+import './InitPage.css';
+
 export default class InitPage extends Component {
   render() {
     return (
       <div>
+        Welcome to the game setup page.
+        <ul className="CategoryList">
+          {categories.map(category => (
+            <li>{category}</li>
+          ))}
+        </ul>
       </div>
     );
   }
