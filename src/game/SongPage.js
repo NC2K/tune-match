ea11024b716c75908388dc457e0adebd33dc8fa9
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-25 13:32:50 -0700

    Setting up SongPage

--- src/game/SongPage.js
+++ src/game/SongPage.js
@@ -4,14 +4,13 @@ import './SongPage.css';
 
 export default class SongPage extends Component {
 
-
   render() {
 
     const songEnd = console.log('The song has finished playing');
     const { song } = 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview124/v4/99/7a/6e/997a6e85-9ffe-cf0b-9023-2f59f7ac6fb9/mzaf_7661357641494777861.plus.aac.p.m4a';
-
     return (
       <div>
+        {/* This plays our song */}
         <figure>
           <figcaption>What is that song?</figcaption>
           <audio
@@ -21,6 +20,8 @@ export default class SongPage extends Component {
             <code>audio</code> element.
           </audio>
         </figure>
+        {/* We need to listen for song onended.*/}
+        {/* On onended, load the next song.*/}
       </div >
     );
   }
