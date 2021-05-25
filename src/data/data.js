ad76a7b9217f42e0268e1f84070e2689cafa0d49
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-25 14:05:41 -0700

    Data set formatted

---
+++ src/data/data.js
@@ -0,0 +1,102 @@
+export let categories = [
+  {
+    category: '50\'s',
+    songs: [
+      'Rudolph,+The+Red-nosed+Reindeer+Gene+Autry',
+      'I+Can+Dream,+Can\'t+I+The+Andrew+Sisters',
+      'Music!+Music!+Music!+Teresa+Brewer',
+      'Mona+Lisa+Nat+King+Cole',
+      'Harbor+Lights+Sammy+Kaye',
+      'Mr.+Sandman+The+Chordettes',
+      'Heartbreak+Hotel+Elvis+Presley',
+      'Tequila+The+Champs',
+      'Witch+Doctor+David+Seville',
+      'The+Purple+People+Eater+Sheb+Wooley',
+      'The+Ballad+of+Davy+Crockett+Bill+Hayes',
+      'Cherry+Pink+And+Apple+Blossom+White+Perez+Prado',
+      'Libson+Antigua+Nelson+Riddle',
+      'Hound+Dog+Elvis+Presley',
+
+    ]
+  },
+  {
+    category: '60\'s',
+    songs: [
+      'Rudolph,+The+Red-nosed+Reindeer+Gene+Autry',
+      'I+Can+Dream,+Can\'t+I+The+Andrew+Sisters',
+      'Music!+Music!+Music!+Teresa+Brewer',
+      'Mona+Lisa+Nat+King+Cole',
+      'Harbor+Lights+Sammy+Kaye',
+      'Mr.+Sandman+The+Chordettes',
+      'Heartbreak+Hotel+Elvis+Presley',
+      'Tequila+The+Champs',
+      'Witch+Doctor+David+Seville',
+      'The+Purple+People+Eater+Sheb+Wooley',
+      'The+Ballad+of+Davy+Crockett+Bill+Hayes',
+      'Cherry+Pink+And+Apple+Blossom+White+Perez+Prado',
+      'Libson+Antigua+Nelson+Riddle',
+      'Hound+Dog+Elvis+Presley',
+
+    ]
+  },
+  {
+    category: '70\'s',
+    songs: [
+      'Rudolph,+The+Red-nosed+Reindeer+Gene+Autry',
+      'I+Can+Dream,+Can\'t+I+The+Andrew+Sisters',
+      'Music!+Music!+Music!+Teresa+Brewer',
+      'Mona+Lisa+Nat+King+Cole',
+      'Harbor+Lights+Sammy+Kaye',
+      'Mr.+Sandman+The+Chordettes',
+      'Heartbreak+Hotel+Elvis+Presley',
+      'Tequila+The+Champs',
+      'Witch+Doctor+David+Seville',
+      'The+Purple+People+Eater+Sheb+Wooley',
+      'The+Ballad+of+Davy+Crockett+Bill+Hayes',
+      'Cherry+Pink+And+Apple+Blossom+White+Perez+Prado',
+      'Libson+Antigua+Nelson+Riddle',
+      'Hound+Dog+Elvis+Presley',
+
+    ]
+  },
+  {
+    category: '80\'s',
+    songs: [
+      'Rudolph,+The+Red-nosed+Reindeer+Gene+Autry',
+      'I+Can+Dream,+Can\'t+I+The+Andrew+Sisters',
+      'Music!+Music!+Music!+Teresa+Brewer',
+      'Mona+Lisa+Nat+King+Cole',
+      'Harbor+Lights+Sammy+Kaye',
+      'Mr.+Sandman+The+Chordettes',
+      'Heartbreak+Hotel+Elvis+Presley',
+      'Tequila+The+Champs',
+      'Witch+Doctor+David+Seville',
+      'The+Purple+People+Eater+Sheb+Wooley',
+      'The+Ballad+of+Davy+Crockett+Bill+Hayes',
+      'Cherry+Pink+And+Apple+Blossom+White+Perez+Prado',
+      'Libson+Antigua+Nelson+Riddle',
+      'Hound+Dog+Elvis+Presley',
+
+    ]
+  },
+  {
+    category: '90\'s',
+    songs: [
+      'Rudolph,+The+Red-nosed+Reindeer+Gene+Autry',
+      'I+Can+Dream,+Can\'t+I+The+Andrew+Sisters',
+      'Music!+Music!+Music!+Teresa+Brewer',
+      'Mona+Lisa+Nat+King+Cole',
+      'Harbor+Lights+Sammy+Kaye',
+      'Mr.+Sandman+The+Chordettes',
+      'Heartbreak+Hotel+Elvis+Presley',
+      'Tequila+The+Champs',
+      'Witch+Doctor+David+Seville',
+      'The+Purple+People+Eater+Sheb+Wooley',
+      'The+Ballad+of+Davy+Crockett+Bill+Hayes',
+      'Cherry+Pink+And+Apple+Blossom+White+Perez+Prado',
+      'Libson+Antigua+Nelson+Riddle',
+      'Hound+Dog+Elvis+Presley',
+
+    ]
+  }
+];
