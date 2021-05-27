1e8fcd85e8265dce0fa54bacb2f3fa4050fb89ee
Casey Cameron <mcasey.cameron@gmail.com>
2021-05-26 17:13:41 -0700

    Conditional Nav Links

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
+  state = {
+    isLoggedIn: false
+  }
+
+  componentDidMount() {
+    const isLoggedIn = window.localStorage.getItem('USER_ID') ? true : false;
+    this.setState({ isLoggedIn });
+  }
 
   render() {
+    const { isLoggedIn } = this.state;
+
+    let nav = '';
+
+    if (isLoggedIn) {
+      nav = <nav>
+        <Link to="/">Home</Link>
+        <Link to="categories">Play game</Link>
+        <Link to="leaderboard">Leaderboard</Link>
+        <Link to="aboutus">About Us</Link>
+      </nav>;
+    } else {
+      nav =
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/auth">Login/Sign Up</Link>
+          <Link to="aboutus">About Us</Link>
+        </nav>;
+    }
+
     return (
       <header className="Header">
 
         <h1>Tune Match</h1>
-
+        <nav>{nav}</nav>
       </header>
     );
   }
