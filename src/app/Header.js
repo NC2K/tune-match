707df4cdb7578e638312542e07957f2063fe664b
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-28 11:24:00 -0700

    Fixed scores put

--- src/app/Header.js
+++ src/app/Header.js
@@ -3,30 +3,21 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
-  state = {
-    isLoggedIn: false
-  }
-
-  componentDidMount() {
-    let isLoggedIn = window.localStorage.getItem('USER_ID') ? true : false;
-    this.setState({ isLoggedIn });
-    this.forceUpdate();
-  }
 
   render() {
-    let { isLoggedIn } = this.state;
+    let { isLoggedIn } = this.props;
 
     let nav = '';
 
     if (isLoggedIn) {
-      nav = 
-      <nav>
-        <NavLink className='glow' to="/">Home</NavLink>
-        <NavLink className='glow' to="categories">Play game</NavLink>
-        <NavLink className='glow' to="resultspage">Collection</NavLink>
-        <NavLink className='glow' to="leaderboard">Leaderboard</NavLink>
-        <NavLink className='glow' to="aboutus">About Us</NavLink>
-      </nav>;
+      nav =
+        <nav>
+          <NavLink className='glow' to="/">Home</NavLink>
+          <NavLink className='glow' to="categories">Play game</NavLink>
+          <NavLink className='glow' to="resultspage">Collection</NavLink>
+          <NavLink className='glow' to="leaderboard">Leaderboard</NavLink>
+          <NavLink className='glow' to="aboutus">About Us</NavLink>
+        </nav>;
     } else {
       nav =
         <nav>
