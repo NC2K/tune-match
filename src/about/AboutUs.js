98e96f64d01b33855ef04e75feccf2d5796255cc
Katherine Tam <katherinemtam@gmail.com>
2021-05-28 04:04:13 -0700

    add rel="noreferrer noopener" to links

--- src/about/AboutUs.js
+++ src/about/AboutUs.js
@@ -18,8 +18,8 @@ export default class AboutUs extends Component {
               <h2>Kalan Prudhomme</h2>
               <p>A tasteful jazz pianist/music sommalier. He likes gorillas, and drawing gorillas.</p>
               <div className="links">
-                <a href="https://github.com/prudhomk" target="_blank"><img src="./../github-logo.png" alt="github"/></a>
-                <a href="https://www.linkedin.com/in/kalanprudhomme/" target="_blank"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
+                <a href="https://github.com/prudhomk" target="_blank" rel="noreferrer noopener"><img src="./../github-logo.png" alt="github"/></a>
+                <a href="https://www.linkedin.com/in/kalanprudhomme/" target="_blank" rel="noreferrer noopener"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
               </div>
             </div>
           </div>
@@ -34,8 +34,8 @@ export default class AboutUs extends Component {
               <h2>Katherine Tam</h2>
               <p>A CSS maestro who sings in the shower. She's gone on tour in Europe and has sung at beautiful venues!</p>
               <div className="links">
-                <a href="https://github.com/katherinemtam" target="_blank"><img src="./../github-logo.png" alt="github"/></a>
-                <a href="https://www.linkedin.com/in/katherinemtam/" target="_blank"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
+                <a href="https://github.com/katherinemtam" target="_blank" rel="noreferrer noopener"><img src="./../github-logo.png" alt="github"/></a>
+                <a href="https://www.linkedin.com/in/katherinemtam/" target="_blank" rel="noreferrer noopener"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
               </div>
             </div>
           </div>
@@ -49,8 +49,8 @@ export default class AboutUs extends Component {
               <h2>Nick Day</h2>
               <p>A test-driven developer, with a penchant for synthesizers.</p>
               <div className="links">
-                <a href="https://www.linkedin.com/in/nick-day-fsd/" target="_blank"><img src="./../github-logo.png" alt="github"/></a>
-                <a href="https://github.com/NickDayFSD" target="_blank"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
+                <a href="https://www.linkedin.com/in/nick-day-fsd/" target="_blank" rel="noreferrer noopener"><img src="./../github-logo.png" alt="github"/></a>
+                <a href="https://github.com/NickDayFSD" target="_blank" rel="noreferrer noopener"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
               </div>
             </div>
           </div>
@@ -63,8 +63,8 @@ export default class AboutUs extends Component {
               <h2>Casey Cameron</h2>
               <p>Playing piano before he could tie his shoes, Casey peforms music and writes scores for videogames.</p>
               <div className="links">
-                <a href="https://github.com/CaseyCameron/" target="_blank"><img src="./../github-logo.png" alt="github"/></a>
-                <a href="https://www.linkedin.com/in/casey-cameron/" target="_blank"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
+                <a href="https://github.com/CaseyCameron/" target="_blank" rel="noreferrer noopener"><img src="./../github-logo.png" alt="github"/></a>
+                <a href="https://www.linkedin.com/in/casey-cameron/" target="_blank" rel="noreferrer noopener"><img src="./../linkedin-logo.png" alt="linkedin"/></a>
               </div>
             </div>
           </div>
